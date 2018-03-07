d6cd4f93c3a44cb28852e4bb050e2fa18a4a58c2
Christian Campos <32370442+christiancampos123@users.noreply.github.com>
2018-03-07 19:52:06 +0100

    Update JS.js

--- JS/JS.js
+++ JS/JS.js
@@ -1,5 +1,5 @@
 //LEER XML de xml/numeracos.xml
-var url="https://rawgit.com/christiancampos123/Formulario/master/XML/XML.xml";
+var url="https://rawgit.com/christiancampos123/Formulario/master/XML/XML2.xml";
 var texto;
 var a = 0;
 var c = 0;
